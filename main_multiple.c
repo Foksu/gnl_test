05bd77ed13a5194b5ede3116434a2046f7d7bb52
Foksu <51400922+Foksu@users.noreply.github.com>
2019-10-31 17:45:00 +0200

    Update main_multiple.c

--- main_multiple.c
+++ main_multiple.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "../get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/uio.h>
