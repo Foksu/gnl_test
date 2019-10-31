cb185ea97bbc5128f078cb16eeb5a86a9f8bfda2
Foksu <51400922+Foksu@users.noreply.github.com>
2019-10-31 17:58:29 +0200

    Update compile_multiple.sh

--- compile_multiple.sh
+++ compile_multiple.sh
@@ -12,7 +12,7 @@
 
 #!/bin/sh
 
-make -C libft/ fclean && make -C libft/
-clang -Wall -Wextra -Werror -g -I libft/includes -o get_next_line.o -c get_next_line.c
-clang -Wall -Wextra -Werror -g -I libft/includes -o main_multiple.o -c main_multiple.c
-clang -o test_gnl_multiple main_multiple.o get_next_line.o -I libft/includes -L libft/ -lft
+make -C ../libft/ fclean && make -C ../libft/
+clang -Wall -Wextra -Werror -g -I ../libft/includes -o ../get_next_line.o -c ../get_next_line.c
+clang -Wall -Wextra -Werror -g -I ../libft/includes -o main_multiple.o -c main_multiple.c
+clang -o test_gnl_multiple main_multiple.o ../get_next_line.o -I ../libft/includes -L ../libft/ -lft
