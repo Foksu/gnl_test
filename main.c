7693c9bbcae58b87c862cfcb928890a34c33fe8b
Foksu <51400922+Foksu@users.noreply.github.com>
2019-10-31 17:46:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <sys/uio.h>
 #include <sys/types.h>
 #include "../get_next_line.h"
-#include "libft/includes/libft.h"
+#include "../libft/includes/libft.h"
 
 int main(int argc, char **argv)
 {
